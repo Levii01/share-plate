2651d3205a812a1a3db4c43345f197bf81ebf12c
Levii01 <m.sedziak01@gmail.com>
2024-06-06 21:05:50 +0200

    Add ability handler for access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :set_paper_trail_whodunnit
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      # render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+      format.json { head :forbidden }
+      format.html { redirect_to main_app.root_url, alert: exception.message }
+      format.js   { render nothing: true, status: :not_found }
+    end
+  end
 end
