15f05afa815956d5abaac1e51dfc1e91f14faa73
Levii01 <m.sedziak01@gmail.com>
2024-06-06 19:01:06 +0200

    Add basic panel for logged user

---
+++ app/helpers/users/panel_helper.rb
@@ -0,0 +1,2 @@
+module Users::PanelHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :user,
              controllers: {
                omniauth_callbacks: 'users/omniauth_callbacks'
              }
-  get 'home/index'
-  root to: 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
+  root to: 'home#index'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'home/index'
+  namespace :users do
+    resources :panel, only: %i[index]
+  end
 end
