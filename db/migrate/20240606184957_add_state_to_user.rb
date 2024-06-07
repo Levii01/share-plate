09c909239543d6f0ab76c29f678dea5206340c6e
Levii01 <m.sedziak01@gmail.com>
2024-06-07 04:38:28 +0200

    Change user migration

--- db/migrate/20240606184957_add_state_to_user.rb
+++ db/migrate/20240606184957_add_state_to_user.rb
@@ -4,7 +4,7 @@ class AddStateToUser < ActiveRecord::Migration[7.1]
   def change
     change_table :users, bulk: true do |t|
       t.string :state, default: :initialized, null: false
-      t.string :type
+      t.string :account_type
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_184957) do
     t.string "uid"
     t.string "avatar_url"
     t.string "state", default: "initialized", null: false
-    t.string "type"
+    t.string "account_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
