ceaf2c0f759f016171870810daccda02869dff9c
Levii01 <m.sedziak01@gmail.com>
2025-03-18 01:22:25 +0100

    Fix assets flaticon and fontAwesome

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,11 +9,11 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap-icons/font')
 Rails.application.config.assets.paths << Rails.root.join('app/assets/fonts')
-Rails.application.config.assets.precompile += %w[.svg .eot .woff .ttf]
+Rails.application.config.assets.precompile += %w[*.eot *.woff *.woff2 *.ttf *.svg *.otf]
 Rails.application.config.assets.debug = true
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-# Rails.application.config.assets.precompile += %w(application.js)
+# Rails.application.config.assets.precompile += %w[application.js]
