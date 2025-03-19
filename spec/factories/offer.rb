7a061d3424382d1693c8fb9fca2581b672963ab0
Levii01 <m.sedziak01@gmail.com>
2025-03-19 03:23:32 +0100

    Update offer factory

--- spec/factories/offer.rb
+++ spec/factories/offer.rb
@@ -6,6 +6,7 @@ FactoryBot.define do
     description { Faker::Lorem.sentence(word_count: 30) }
     available_from { Time.zone.now }
     available_to { 1.day.from_now }
+    initial_quantity { rand(2..7) }
     # status {}
     food_provider
   end
