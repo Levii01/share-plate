bc2168b3f33a48981ecfb1067d232f2bcd04bbf2
Levii01 <m.sedziak01@gmail.com>
2025-03-19 05:13:38 +0100

    Add paper trail to offer and reservation

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Offer < ApplicationRecord
+  has_paper_trail
+
   belongs_to :food_provider
 
   validates :name, presence: true
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Reservation < ApplicationRecord
+  has_paper_trail
+
   belongs_to :offer
   belongs_to :beneficiary
 
