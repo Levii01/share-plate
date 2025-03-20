8d9034b8b680a7e1139dae6188a6d294786e3445
Levii01 <m.sedziak01@gmail.com>
2025-03-20 19:13:49 +0100

    Refactor edit food provider

--- app/controllers/users/food_providers_controller.rb
+++ app/controllers/users/food_providers_controller.rb
@@ -4,20 +4,13 @@ module Users
   class FoodProvidersController < ApplicationController
     before_action :food_provider, only: %i[edit update]
 
-    # GET /food_providers/1/edit
     def edit; end
 
     def update
-      respond_to do |format|
-        if food_provider.update(food_provider_params)
-          format.html do
-            redirect_to users_registrations_food_providers_path(food_provider), notice: 'Dane lokalu zostały zapisane'
-          end
-          format.json { render :new, status: :ok, location: food_provider }
-        else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: food_provider.errors, status: :unprocessable_entity }
-        end
+      if food_provider.update(food_provider_params)
+        redirect_to users_food_provider_path, notice: 'Dane lokalu zostały zapisane'
+      else
+        render :edit, status: :unprocessable_entity
       end
     end
 
